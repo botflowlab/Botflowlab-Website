315050e7c7a8283d87770cb37aab31b73f3ab9ed
botflowlab <botflowlab@gmail.com>
2025-07-22 17:05:58 -0600

    Updated SuccessCasesSection.tsx

--- src/components/sections/SuccessCasesSection.tsx
+++ src/components/sections/SuccessCasesSection.tsx
@@ -5,7 +5,7 @@ import { SuccessCasesGrid } from './success-cases/SuccessCasesGrid';
 
 export const SuccessCasesSection: React.FC<{ isVisible: boolean }> = ({ isVisible }) => {
   return (
-    <div id="portfolio" className="relative min-h-screen bg-black overflow-hidden py-32">
+    <div id="portfolio" className="relative bg-black overflow-hidden py-32">
       <div className="max-w-[1400px] mx-auto px-6 relative">
         <SuccessCasesHeader isVisible={isVisible} />
         <SuccessCasesGrid isVisible={isVisible} />
